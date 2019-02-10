8325cab690fc7a605515a6cd578f4b567d7b21cc
Ferdinand Malcher <mail@ferdinand-malcher.de>
2019-02-10 13:08:09 +0100

    Change book metadata, remove english books
    @johanneshoppe @d-koppenhagen

--- src/model/some-books.ts
+++ src/model/some-books.ts
@@ -1,11 +1,8 @@
 ﻿import { Book, Thumbnail } from './book'
 
 export class SomeBooks {
-  public static get() :Book[] {
-    return [
-      ...SomeBooks.germanBooks,
-      // ...SomeBooks.englishBooks
-    ];
+  public static get(): Book[] {
+    return SomeBooks.germanBooks;
   }
 
   public static secureBook: Book =
@@ -20,54 +17,11 @@ export class SomeBooks {
       `Wenn Sie diesen Text lesen können, haben Sie den Authorization-Header korrekt gesendet - denn dieses geheime Buch ist nur über die sichere Ressource verfügbar. Es kann weder neu angelegt, noch verändert oder gelöscht werden.`
     );
 
-  public static englishBooks: Book[] = [
-    new Book(
-      '9781788620703',
-      'Switching to Angular - Third Edition',
-      ['Minko Gechev'],
-      new Date('2017-10-01T12:00:00.000Z'),
-      `Align with Google's long-term vision for Angular version 5 and beyond`,
-      5,
-      [new Thumbnail('https://d255esdrn735hr.cloudfront.net/sites/default/files/imagecache/ppv4_main_book_cover/B09239_cover.png', 'Front Cover')],
-      `Switching to Angular, Third Edition is the go-to book to align and get started with the Angular JavaScript framework. Angular contributor and international speaker Minko Gechev will help you square up and start building Angular apps and provide you an insight to the Google’s vision for the framework. `
-    ),
-    new Book(
-      '9781787124929',
-      'Learning Angular - Second Edition',
-      ['Christoffer Noring', 'Pablo Deeleman'],
-      new Date('2017-12-01T12:00:00.000Z'),
-      `Build modern SPAs by learning the latest and powerful features of Angular 5 and TypeScript 2.x `,
-      4,
-      [new Thumbnail('https://www.packtpub.com/sites/default/files/B06169_cover.png', 'Front Cover')],
-      `The latest version of Angular comes with a lot of new features that help you to make your applications smaller and faster. This book will show you how to set up an Angular project, and you’ll build Angular components right from the beginning. Moving on, you’ll explore and work with the components to build your app. Next, you’ll find out more about TypeScript and see how to use it to build apps in the best way possible. You’ll then be introduced to the building blocks - Properties, Events, Directives, and Pipes - and how it can be used to implement and enhance the components.`
-    ),
-    new Book(
-      '9781787124929',
-      'Architecting Angular Applications with Redux, RxJS, and NgRx',
-      ['Christoffer Noring'],
-      new Date('2017-12-01T12:00:00.000Z'),
-      `Manage state in Angular to write high performing web apps by combining the power of Flux, RxJS, and NgRx `,
-      5,
-      [new Thumbnail('https://www.packtpub.com/sites/default/files/B07031_cover.png', 'Front Cover')],
-      `Managing the state of large-scale web applications is a highly challenging task with the need to align different components, backends, and web workers harmoniously.  When it comes to Angular, you can use NgRx, which combines the simplicity of Redux with the reactive programming power of RxJS to build your application architecture, making your code elegant and easy to reason about, debug, and test. In this book, we start by looking at the different ways of architecting Angular applications and some of the patterns that are involved in it. This will be followed by a discussion on one-way data flow, the Flux pattern, and the origin of Redux.`
-    ),
-    new Book(
-      '9781680501735',
-      'Reactive Programming with RxJS',
-      ['Sergi Mansilla'],
-      new Date('2015-12-01T12:00:00.000Z'),
-      `Untangle your asynchronous JavaScript code`,
-      3,
-      [new Thumbnail('https://www.safaribooksonline.com/library/cover/9781680501735/360h/', 'Front Cover')],
-      `Reactive programming is revolutionary. It makes asynchronous programming cleaner, intuitive, and robust. Discover how to use the RxJS library to write programs in a simpler way, unifying asynchronous mechanisms such as callbacks and promises into a single, powerful construct. Learn to think about your programs as streams of data that you can transform by expressing what should happen, instead of having to painstakingly program how it should happen. You'll be able to handle real-world concurrency and write complex flows of events in your applications with ease.`
-    )
-  ];
-
   public static germanBooks: Book[] = [
     new Book(
       '9783864906466',
       'Angular',
-      ['Johannes Hoppe', 'Ferdinand Malcher', 'Danny Koppenhagen'],
+      ['Ferdinand Malcher', 'Johannes Hoppe', 'Danny Koppenhagen'],
       new Date('2019-04-30T00:00:00.000Z'),
       'Grundlagen, fortgeschrittene Themen und Best Practices – mit NativeScript und NgRx',
       5,
@@ -83,8 +37,8 @@ export class SomeBooks {
     new Book(
       '9783864903571',
       'Angular',
-      ['Johannes Hoppe', 'Ferdinand Malcher', 'Danny Koppenhagen', 'Gregor Woiwode'],
-      new Date('2017-04-01T12:00:00.000Z'),
+      ['Gregor Woiwode', 'Ferdinand Malcher', 'Danny Koppenhagen', 'Johannes Hoppe'],
+      new Date('2017-05-22T12:00:00.000Z'),
       'Grundlagen, fortgeschrittene Techniken und Best Practices mit TypeScript - ab Angular 4, inklusive NativeScript und Redux',
       5,
       [new Thumbnail('https://api3.angular-buch.com/images/angular_auflage1.jpg', 'Front Cover')],
