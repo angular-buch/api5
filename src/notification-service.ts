429b1ed6037bd8d177f309a76743a5a63e832049
Ferdinand Malcher <ferdinand@malcher.media>
2022-07-06 22:17:10 +0200

    korrektur

--- src/notification-service.ts
+++ src/notification-service.ts
@@ -2,7 +2,7 @@ import * as _ from 'lodash';
 import { WebPushError, PushSubscription, sendNotification, setVapidDetails } from 'web-push';
 export class NotificationService {
   private subscriptions: PushSubscription[] = [];
-  private subscriptionOriginMap = new Map()
+  private subscriptionOriginMap = new Map();
 
   constructor() {
     const vapidKeys = {
@@ -19,8 +19,8 @@ export class NotificationService {
 
   addSubscription(subscription: PushSubscription, origin: string) {
     this.subscriptions.push(subscription);
-    this.subscriptions = _.uniq(this.subscriptions); // remove duplicate subscriptions if exists
-    this.subscriptionOriginMap.set(subscription.endpoint, origin) // store Subscribers origin info for this endpoint
+    this.subscriptions = _.uniq(this.subscriptions); // remove possible duplicate subscriptions
+    this.subscriptionOriginMap.set(subscription.endpoint, origin) // store subscriber's origin info for this endpoint. We need this to create URLs for the client app.
   }
 
   hasSubscriber(): boolean {
@@ -29,22 +29,28 @@ export class NotificationService {
 
   notifySubscribers(payload: NotificationOptions) {
     this.subscriptions.forEach(sub => {
-      const origin = this.subscriptionOriginMap.get(sub.endpoint)
-      const notificationPayload = {
-        ...payload,
-        data: {
+      const origin = this.subscriptionOriginMap.get(sub.endpoint);
+      const notificationPayload = { ...payload };
+
+      // when there is a book in the payload, add click action to open detail page
+      if (payload.data?.book) {
+        notificationPayload.data = {
           book: payload.data.book,
           onActionClick: {
-            default: { operation: "openWindow", url: `${origin}/books/${payload.data.book.isbn}` },
+            default: {
+              operation: 'openWindow',
+              url: `${origin}/books/${payload.data.book.isbn}`
+            }
           }
-        }
+        };
       }
-      this.notify(notificationPayload, sub)
+
+      this.notify(notificationPayload, sub);
     });
   }
 
-  notify(payload: NotificationOptions, subscription: PushSubscription) {
-    sendNotification(subscription, JSON.stringify({ notification: payload }))
+  notify(notification: NotificationOptions, subscription: PushSubscription) {
+    sendNotification(subscription, JSON.stringify({ notification }))
       .catch((error: WebPushError) => console.error(error));
   }
 }
